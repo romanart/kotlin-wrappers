d6532022db6373718a986c81c105478bd21f5d89
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2019-11-20 19:16:40 +0300

    JS IR Build

--- kotlin-redux/src/main/kotlin/redux/Serialization.kt
+++ kotlin-redux/src/main/kotlin/redux/Serialization.kt
@@ -2,17 +2,19 @@
 
 package redux
 
-import kotlinx.serialization.*
-import kotlinx.serialization.internal.*
-import kotlinx.serialization.json.*
+// TODO: Support JS IR version of kotlinx.serialization
 
-@UseExperimental(ImplicitReflectionSerializer::class)
-inline fun <reified A : RAction> serializeAction(action: A): String {
-    return "{\"type\":\"${A::class.simpleName}\",${Json.stringify(action).drop(1)}"
-}
-
-@UseExperimental(ImplicitReflectionSerializer::class)
-inline fun <reified A : RAction> deserializeAction(action: String): A? {
-    val map = Json.parse((StringSerializer to StringSerializer).map, action)
-    return if (map["type"] == A::class.simpleName) Json.nonstrict.parse(action) else null
-}
+// import kotlinx.serialization.*
+// import kotlinx.serialization.internal.*
+// import kotlinx.serialization.json.*
+//
+// @UseExperimental(ImplicitReflectionSerializer::class)
+// inline fun <reified A : RAction> serializeAction(action: A): String {
+//     return "{\"type\":\"${A::class.simpleName}\",${Json.stringify(action).drop(1)}"
+// }
+//
+// @UseExperimental(ImplicitReflectionSerializer::class)
+// inline fun <reified A : RAction> deserializeAction(action: String): A? {
+//     val map = Json.parse((StringSerializer to StringSerializer).map, action)
+//     return if (map["type"] == A::class.simpleName) Json.nonstrict.parse(action) else null
+// }
